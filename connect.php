adf14f2c56a6484cd7635e5bb46d8612cc0f6256
j-hamilton3 <118838540+j-hamilton3@users.noreply.github.com>
2023-11-04 15:15:50 -0500

    Added DB connection authentication to all pages and created HTML shells.

---
+++ connect.php
@@ -0,0 +1,28 @@
+<?php
+
+/*******w******** 
+    
+    Name: James Hamilton
+    Date: November 4, 2023
+    Description: Created to ensure the user is connected to the Database.
+
+****************/
+
+     define('DB_DSN','mysql:host=localhost;dbname=serverside;charset=utf8');
+     define('DB_USER','serveruser');
+     define('DB_PASS','gorgonzola7!');     
+     
+    //  PDO is PHP Data Objects
+    //  mysqli <-- BAD. 
+    //  PDO <-- GOOD.
+     try {
+         // Try creating new PDO connection to MySQL.
+         $db = new PDO(DB_DSN, DB_USER, DB_PASS);
+         //,array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION)
+     } catch (PDOException $e) {
+         print "Error: " . $e->getMessage();
+         die(); // Force execution to stop on errors.
+         // When deploying to production you should handle this
+         // situation more gracefully. ¯\_(ツ)_/¯
+     }
+ ?>
