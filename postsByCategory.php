b80324682099d7a37dc16284db0e97cccc9fb838
j-hamilton3 <118838540+j-hamilton3@users.noreply.github.com>
2024-02-03 15:00:31 -0600

    Fixed postsByCategory styling.

--- postsByCategory.php
+++ postsByCategory.php
@@ -1,15 +1,15 @@
 <?php
 
-/*******w******** 
-    
+/*******w********
+
     Name: James Hamilton
     Date: November 14, 2023
     Description: A webpage for MyBassGallery that allows users to view a specific category.
 
 ****************/
-   
+
     // There must be a DB connection to continue.
-    require('connect.php'); 
+    require('connect.php');
 
     // Include the utility functions file.
     require('utility.php');
@@ -18,7 +18,7 @@
     session_start();
 
     if (isset($_GET['id'])) {
-        
+
         // Sanitize the id from the input GET.
         $id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
 
@@ -27,12 +27,12 @@
         $statement = $db->prepare($query);
         $statement->bindValue(':id', $id, PDO::PARAM_INT);
 
-       
+
         $statement->execute();
-    
+
         // If the $id is false.
-    } 
-    else 
+    }
+    else
     {
         $id = false;
     }
@@ -66,9 +66,9 @@
         <?php if(empty($_SESSION)) : ?>
             <div class="links">
                 <a href="categories.php">Categories</a>
-                <a href="register.php">Register</a> 
-                <a href="login.php">Login</a>   
-            </div>  
+                <a href="register.php">Register</a>
+                <a href="login.php">Login</a>
+            </div>
         <?php else : ?>
             <div class="links">
                 <a href="create.php">Create a Post</a>
@@ -80,21 +80,21 @@
                 <a class="username"><?= $_SESSION['user']['userName'] ?></a>
                 <a href="login.php">Logout</a>
             </div>
-        <?php endif ?>   
+        <?php endif ?>
     </nav>
     <?php if($id) : ?>
-        <h1>Category: <?= getCategoryByID($id, $db)['categoryName']  ?> Basses</h1>
+        <h1 id="category-posts-title">Category: <?= getCategoryByID($id, $db)['categoryName']  ?> Basses</h1>
         <?php while($post = $statement->fetch()) : ?>
         <div class="post">
             <h2><a href="post.php?id=<?= $post['postID'] ?>"><?= $post['title'] ?></a></h2>
-            <p>Serial Number: <?= $post['serialNumber'] ?></p>
+            <p><b>Serial Number:</b> <?= $post['serialNumber'] ?></p>
             <?php if(!empty($post['image'])) : ?>
                 <img src="<?= $post['image'] ?>" class="post-image">
             <?php endif ?>
             <p><?= html_entity_decode($post['content']) ?></p>
-            <p>Category: <?= getCategoryByID($post['categoryID'], $db)['categoryName'] ?></p>
-            <p>Post by user: <?= getUserByID($post['userID'], $db)['userName'] ?></p>
-            <p>Created on: <?= $post['date'] ?></p>
+            <p><b>Category:</b> <?= getCategoryByID($post['categoryID'], $db)['categoryName'] ?></p>
+            <p><b>Post by user:</b> <?= getUserByID($post['userID'], $db)['userName'] ?></p>
+            <p><b>Created On:</b> <?= date("F j, Y g:i A", strtotime($post['date'])) ?></p>
         </div>
     <?php endwhile ?>
     <?php else : ?>
